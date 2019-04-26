765dfe41079202b5f5b1de04701cebff4ebec8cd
kamaboko1 <github65@hotmail.com>
2019-04-26 09:26:29 -0700

    setup profiles in characters component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Character = ({chars}) => {
   return (
     <div>
-      <p>{chars.name}</p>
+      <div className="character--card">
+        <h3></h3>
+        <div className="character--card-content">
+            <div className="charcter--card-stats">
+                <p><span>Gender:</span> {chars.gender}</p>
+                <p><span>Birth year:</span> {chars.birth_year}</p>
+                <p><span>Hair color:</span>{chars.hair_color}</p>
+            </div>
+            <div className="charcter--card-stats">
+                <p><span>Eye color:</span> {chars.eye_color}</p>
+                <p><span>Height:</span> {chars.height}</p>
+                <p><span>Skin:</span> {chars.skin_coor}</p>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
