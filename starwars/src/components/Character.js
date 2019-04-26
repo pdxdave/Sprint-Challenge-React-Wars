54c6f370225ade4656c5f99c01031383d8ea1d5b
kamaboko1 <github65@hotmail.com>
2019-04-26 09:42:32 -0700

    basic card layout

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,20 +1,21 @@
 import React from 'react'
+import './StarWars.css';
 
 const Character = ({chars}) => {
   return (
     <div>
       <div className="character--card">
-        <h3></h3>
+        <h3>{chars.name}</h3>
         <div className="character--card-content">
-            <div className="charcter--card-stats">
+            <div className="character--card-stats">
                 <p><span>Gender:</span> {chars.gender}</p>
                 <p><span>Birth year:</span> {chars.birth_year}</p>
                 <p><span>Hair color:</span>{chars.hair_color}</p>
             </div>
-            <div className="charcter--card-stats">
+            <div className="character--card-stats">
                 <p><span>Eye color:</span> {chars.eye_color}</p>
                 <p><span>Height:</span> {chars.height}</p>
-                <p><span>Skin:</span> {chars.skin_coor}</p>
+                <p><span>Skin:</span> {chars.skin_color}</p>
             </div>
         </div>
       </div>
