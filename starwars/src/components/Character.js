046319a719d24434fbe2c1cc41d430ff2061c26f
kamaboko1 <github65@hotmail.com>
2019-04-26 08:13:09 -0700

    got components talking to each other

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Character = () => {
+  return (
+    <div>
+      <h1>Character</h1>
+    </div>
+  )
+}
+
+export default Character;
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Character from './Character';
+
+ const CharacterList = () => {
+  return (
+    <div>
+      <Character />
+    </div>
+  )
+}
+
+export default CharacterList;
